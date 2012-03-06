fe0796dffea608876806518befa8bbc6d24f49f3
graemerocher <graeme.rocher@gmail.com>
2012-03-06 11:33:42 +0100

    updated to 1.0

--- RestClientBuilderGrailsPlugin.groovy
+++ RestClientBuilderGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RestClientBuilderGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
