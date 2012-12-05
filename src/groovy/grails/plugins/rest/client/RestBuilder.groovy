0aa2164b433198a4bd8b63fba099745277700250
Burt Beckwith <beckwithb@vmware.com>
2012-12-04 19:33:06 -0500

    moved ErrorResponse, RequestCustomizer, and RestResponse classes into their own files

--- src/groovy/grails/plugins/rest/client/RestBuilder.groovy
+++ src/groovy/grails/plugins/rest/client/RestBuilder.groovy
@@ -1,54 +1,38 @@
 package grails.plugins.rest.client
 
 import static org.springframework.http.MediaType.*
-import grails.converters.JSON
-import grails.converters.XML
-import grails.web.JSONBuilder
-import groovy.util.slurpersupport.GPathResult
 
-import org.codehaus.groovy.grails.plugins.codecs.Base64Codec
-import org.codehaus.groovy.grails.web.json.JSONElement
-import org.springframework.core.io.FileSystemResource
-import org.springframework.core.io.InputStreamResource
-import org.springframework.core.io.UrlResource
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.client.SimpleClientHttpRequestFactory
-import org.springframework.util.LinkedMultiValueMap
-import org.springframework.util.MultiValueMap
+import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 
 class RestBuilder {
 
-    RestTemplate restTemplate
+    RestTemplate restTemplate = new RestTemplate()
 
-    RestBuilder() {
-        restTemplate = new RestTemplate()
-    }
+    RestBuilder() {}
 
     RestBuilder(Map settings) {
-        this()
 
         def proxyHost = System.getProperty("http.proxyHost")
         def proxyPort = System.getProperty("http.proxyPort")
 
-        if(proxyHost && proxyPort) {
-            if(settings.proxy == null) {
+        if (proxyHost && proxyPort) {
+            if (settings.proxy == null) {
                 settings.proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort.toInteger()))
             }
         }
-        if(settings.proxy instanceof Map) {
+
+        if (settings.proxy instanceof Map) {
             def ps = settings.proxy.entrySet().iterator().next()
-            if(ps.value) {
+            if (ps.value) {
                 def proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ps.key, ps.value.toInteger()))
                 settings.proxy = proxy
             }
         }
 
-        restTemplate = new RestTemplate()
         restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory(settings))
     }
 
@@ -57,8 +41,8 @@ class RestBuilder {
      * @param url The URL
      * @param url The closure customizer used to customize request attributes
      */
-    def get(String url, Closure customizer=null) {
-        doRequestInternal( url, customizer, HttpMethod.GET)
+    def get(String url, Closure customizer = null) {
+        doRequestInternal(url, customizer, HttpMethod.GET)
     }
 
     /**
@@ -68,7 +52,7 @@ class RestBuilder {
      * @param customizer The clouser customizer
      */
     def put(String url, Closure customizer = null) {
-        doRequestInternal( url, customizer, HttpMethod.PUT)
+        doRequestInternal(url, customizer, HttpMethod.PUT)
     }
 
     /**
@@ -77,7 +61,7 @@ class RestBuilder {
      * @param customizer (optional) The closure customizer
      */
     def post(String url, Closure customizer = null) {
-        doRequestInternal( url, customizer, HttpMethod.POST)
+        doRequestInternal(url, customizer, HttpMethod.POST)
     }
 
     /**
@@ -87,200 +71,28 @@ class RestBuilder {
      * @param customizer (optional) The closure customizer
      */
     def delete(String url, Closure customizer = null) {
-        doRequestInternal( url, customizer, HttpMethod.DELETE)
+        doRequestInternal(url, customizer, HttpMethod.DELETE)
     }
 
     protected doRequestInternal(String url, Closure customizer, HttpMethod method) {
 
         def requestCustomizer = new RequestCustomizer()
-        if(customizer != null) {
+        if (customizer != null) {
             customizer.delegate = requestCustomizer
             customizer.call()
         }
+
         try {
-            def responseEntity = restTemplate.exchange(url, method,requestCustomizer.createEntity(),
+            def responseEntity = restTemplate.exchange(url, method, requestCustomizer.createEntity(),
                     String, requestCustomizer.getVariables())
             handleResponse(responseEntity)
         }
-        catch(org.springframework.web.client.HttpStatusCodeException e) {
+        catch (HttpStatusCodeException e) {
             return new ErrorResponse(error:e)
         }
     }
+
     protected handleResponse(ResponseEntity responseEntity) {
         return new RestResponse(responseEntity: responseEntity)
-    }
-}
-
-class ErrorResponse {
-    @Delegate org.springframework.web.client.HttpStatusCodeException error
-    @Lazy String text = {
-        error.responseBodyAsString
-    }()
-    @Lazy JSONElement json = {
-        def body = error.responseBodyAsString
-        if(body) {
-            return JSON.parse(body)
-        }
-    }()
-    @Lazy GPathResult xml = {
-        def body = error.responseBodyAsString
-        if(body) {
-            return XML.parse(body)
-        }
-    }()     
-    
-    byte[] getBody() {
-        error.responseBodyAsByteArray
-    }
-
-    int getStatus() {
-        error.statusCode?.value() ?: 200
-    }
-}
-
-class RestResponse {
-    @Delegate ResponseEntity responseEntity
-    @Lazy JSONElement json = {
-        def body = responseEntity.body
-        if(body) {
-            return JSON.parse(body)
-        }
-    }()
-    @Lazy GPathResult xml = {
-        def body = responseEntity.body
-        if(body) {
-            return XML.parse(body)
-        }
-    }()
-
-    @Lazy String text = {
-        def body = responseEntity.body
-        if(body) {
-            return body.toString()
-        }
-        else {
-            responseEntity.statusCode.reasonPhrase
-        }
-    }()
-
-    int getStatus() {
-        responseEntity?.statusCode?.value() ?: 200
-    }
-}
-
-class RequestCustomizer {
-    HttpHeaders headers = new HttpHeaders()
-    def body
-    MultiValueMap<String, Object> mvm = new LinkedMultiValueMap<String, Object>()
-    Map<String, Object> variables = [:]
-
-    // configures basic author
-    RequestCustomizer auth(String username, String password) {
-        String authStr = "$username:$password"
-        String encoded = Base64Codec.encode(authStr)
-        headers["Authorization"] = "Basic $encoded".toString()
-        return this
-    }
-
-    RequestCustomizer contentType(String contentType) {
-        headers.setContentType(MediaType.valueOf(contentType))
-        return this
-    }
-
-    RequestCustomizer accept(String...contentTypes) {
-        def list = contentTypes.collect { MediaType.valueOf(it) }
-        headers.setAccept(list)
-        return this
-    }
-
-    RequestCustomizer header(String name, String value) {
-        headers[name] = value
-        return this
-    }
-
-    RequestCustomizer json(Closure callable) {
-        def builder = new JSONBuilder()
-        callable.resolveStrategy = Closure.DELEGATE_FIRST
-        JSON j = builder.build(callable) 
-        json(j)
-    }
-
-    RequestCustomizer json(JSON json) {
-        body = json.toString()
-        if(!headers.contentType) {
-            contentType "application/json"
-        }        
-        return this
-    }    
-    RequestCustomizer json(String json) {
-        body = json
-        if(!headers.contentType) {
-            contentType "application/json"
-        }        
-        return this
-    }       
-    RequestCustomizer json(object) {
-        def json = object as JSON
-        body = json.toString()
-        if(!headers.contentType) {
-            contentType "application/json"
-        }        
-        return this
-    }      
-
-    RequestCustomizer xml(Closure closure) {
-        def b = new groovy.xml.StreamingMarkupBuilder()
-        def markup = b.bind(closure)
-        def StringWriter sw = new StringWriter()
-        markup.writeTo(sw)
-        this.body = sw.toString()
-        return this
-    }
-
-    RequestCustomizer xml(object) {
-        def xml = object as XML
-        this.body = xml.toString()
-        return this
-    }
-    RequestCustomizer urlVariables(Map<String, Object> variables) {
-        if (variables!=null)
-            this.variables = variables
-        return this
-    }
-
-    RequestCustomizer body(content) {
-        if(content instanceof JSON) {
-            if(!headers.contentType) {
-                contentType "application/json"
-            }
-            this.body = content.toString()
-        }
-        else {
-            this.body = content    
-        }
-
-        return this
-    }
-
-    HttpEntity createEntity() {
-        if(mvm) {
-            return new HttpEntity(mvm, headers)
-        }
-        else {
-            return new HttpEntity(body, headers)
-        }
-    }
-
-    void setProperty(String name, value) {
-        if(value instanceof File) {
-            value = new FileSystemResource(value)
-        }
-        else if(value instanceof URL) {
-            value = new UrlResource(value)
-        }
-        else if(value instanceof InputStream) {
-            value = new InputStreamResource(value)
-        }
-        mvm[name] = value
     }
 }
