22332eb8604bab6359bc2e4ec559384d1068b96c
Chris Love <clove@cnmconsulting.net>
2013-10-17 17:20:06 -0600

    fixing pom file and other stuff so this will deploy

--- RestClientBuilderGrailsPlugin.groovy
+++ RestClientBuilderGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RestClientBuilderGrailsPlugin {
 	def version = "1.0.4"
-	def grailsVersion = "2.0 > *"
+	def grailsVersion = "2.2.3 > *"
 
 	def title = 'REST Client Builder Plugin'
 	def description = 'Grails REST Client Builder Plugin'
